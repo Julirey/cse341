2019ceadd8015dd828f79934ebe7fe910431dbd4
Julirey <reyes.0271204@gmail.com>
2024-11-09 18:10:09 -0400

    Remove test console log

--- backend/controllers/professionalController.js
+++ backend/controllers/professionalController.js
@@ -4,7 +4,6 @@ const getData = async (req, res, next) => {
   const result = await mongodb.getDb().db("test").collection("user").find();
 
   result.toArray().then((lists) => {
-    console.log(lists[0]);
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists[0]);
   });
